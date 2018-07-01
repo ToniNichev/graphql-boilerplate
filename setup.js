f880772c77acb6aa10c7521e06549d51dbb3c707
ToniNichev <toni.nichev@gmail.com>
2018-07-01 13:48:43 -0400

    #stable

--- setup.js
+++ setup.js
@@ -1,4 +1,4 @@
 const mongoDB = require('./src/connectors/database/mongodb');
 const dogs = require('./src/models/mock_data/dogs');
 
-mongoDB.add('Dogs', dogs, null);
+mongoDB.add(dogs, 'Dogs', null);
