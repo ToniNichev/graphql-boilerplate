7b9ba4f0f5b9665c444e04c9f4fa919375a21a94
ToniNichev <toni.nichev@gmail.com>
2018-07-05 20:23:38 -0400

    #stable - added pages

--- setup.js
+++ setup.js
@@ -1,4 +1,7 @@
 const mongoDB = require('./src/connectors/database/mongodb');
 const dogs = require('./src/models/mock_data/dogs');
+const pages = require('./src/models/mock_data/pages');
 
 mongoDB.add(dogs, 'Dogs', null);
+
+mongoDB.add(pages, 'Pages', null);
---
+++ src/models/mock_data/pages.js
@@ -0,0 +1,17 @@
+module.exports = {
+    id: '12214w',
+    url: 'one',
+    layout: [ 
+      {
+        span: '8',
+        components:[
+          {
+            name: "DogsCatalogWithRedux"
+          },
+          {
+            name: "GraphqlExample"
+          }
+        ] 
+      },                  
+    ]
+  }
