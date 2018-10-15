6c22b1e72474c9581774864d366d2a92db80bf9b
ToniNichev <toni.nichev@gmail.com>
2018-10-15 13:36:54 -0400

    Adding logger

--- server.js
+++ server.js
@@ -3,10 +3,22 @@ const cors = require('cors');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./src/schema');
 
+// Your own super cool function
+var logger = function(req, res, next) {
+  debugger;
+  console.log("GOT REQUEST >", req.url);
+  next(); // Passing the request to the next handler in the stack.
+}
+
 const app = express();
+app.use(logger); // Here you add your logger to the stack.
+
 app.use('/graphql', cors(), graphqlHTTP({
   schema: schema,
   graphiql: true,
 }));
+
+
+
 app.listen(4001);
 console.log('Running a GraphQL API server at localhost:4001/graphql');
