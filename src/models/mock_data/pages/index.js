33e36d30ccf746a50bf933493876033e04cda54b
ToniNichev <toni.nichev@gmail.com>
2019-01-04 15:54:53 -0500

    Work

--- src/models/mock_data/pages/index.js
+++ src/models/mock_data/pages/index.js
@@ -2,6 +2,7 @@ const home = require('./home');
 const dogsCatalog = require('./dogs-catalog');
 const todo = require('./todo');
 const about = require('./about');
+const aboutshort = require('./aboutshort');
 const gallery = require('./gallery');
 const greetings = require('./greetings');
 const ChartComponent = require('./ChartComponent');
@@ -11,6 +12,7 @@ module.exports = home.concat(
     dogsCatalog, 
     todo, 
     about, 
+    aboutshort,
     gallery,
     ChartComponent,
     greetings
